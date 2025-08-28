db83533c7854c22d4f2e484d39574559cc358a16
developertarekrahman <tarekrahmanuiux@gmail.com>
2025-08-29 01:35:50 +0200

    Copy functionalities done

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ for(let heart of hearts){
     })
 }
 
-//coin functionalities
+//call functionalities
 
 
 let coin=100;
@@ -24,7 +24,7 @@ for (let i=0; i<callbtn.length;i++){
     callbtn[i].addEventListener('click',function(){
         if (coin>=20){
             alert(`Calling ${names[i].innerText} at ${numbers[i].innerText}`);
-            coins=coins-20;
+            coin=coin-20;
             coinCount.innerText=coin;
         }
         else{
@@ -32,4 +32,37 @@ for (let i=0; i<callbtn.length;i++){
         }
     })
 }
+
+
+//copy section
+
+let number = 0;
+const counter = document.getElementById("count-copy");
+const copies = document.getElementsByClassName("copy-button");
+for(let copy of copies){
+    copy.addEventListener('click',function(){
+        alert(`The number is copied`)
+        number++;
+        counter.innerText=number;
+
+    })
+}
+
+
+const copyButtons = document.getElementsByClassName('copy-button');
+for (let i = 0; i < copyButtons.length; i++) {
+  copyButtons[i].onclick = function() {
+    const card = this.closest('.carts');
+
+    const number = card.querySelector('.number').innerText;
+    navigator.clipboard.writeText(number)
+      .catch(err => console.error('Failed to copy number:', err));
+  };
+}
+
+
+
+
+//history section
+
 
