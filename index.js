d30952853dfeb2899c92ec519ff0dd6990b4eb1c
developertarekrahman <tarekrahmanuiux@gmail.com>
2025-08-28 23:05:19 +0200

    call functionalities finished

--- index.js
+++ index.js
@@ -9,3 +9,27 @@ for(let heart of hearts){
         count.innerText=num;
     })
 }
+
+//coin functionalities
+
+
+let coin=100;
+let coinCount= document.getElementById('coins');
+coinCount.innerText=coin;
+const numbers=document.getElementsByClassName('number');
+const names=document.getElementsByClassName('name');
+const callbtn= document.getElementsByClassName('call-button');
+
+for (let i=0; i<callbtn.length;i++){
+    callbtn[i].addEventListener('click',function(){
+        if (coin>=20){
+            alert(`Calling ${names[i].innerText} at ${numbers[i].innerText}`);
+            coins=coins-20;
+            coinCount.innerText=coin;
+        }
+        else{
+            alert("You have no sufiicient coin to call")
+        }
+    })
+}
+
